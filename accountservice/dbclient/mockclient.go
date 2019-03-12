5612f188d5de2610e793ced0e2edde0ffce19a25
Hoang Quoc Vinh Nguyen <quocvinh.nguyen@grabtaxi.com>
2019-03-12 18:39:49 +0700

    init

---
+++ accountservice/dbclient/mockclient.go
@@ -0,0 +1,28 @@
+package dbclient
+
+import (
+	"github.com/stretchr/testify/mock"
+	"github.com/vinhnguyenhq/goblog/accountservice/model"
+)
+
+// MockBoltClient is a mock implementation of a datastore client for testing purposes.
+// Instead of the bolt.DB pointer, we're just putting a generic mock object from
+// strechr/testify
+type MockBoltClient struct {
+	mock.Mock
+}
+
+// From here, we'll declare three functions that makes our
+// MockBoltClient fulfill the interface IBoltClient that we declared in part 3.
+func (m *MockBoltClient) QueryAccount(accountId string) (model.Account, error) {
+	args := m.Mock.Called(accountId)
+	return args.Get(0).(model.Account), args.Error(1)
+}
+
+func (m *MockBoltClient) OpenBoltDb() {
+	// Does nothing
+}
+
+func (m *MockBoltClient) Seed() {
+	// Does nothing
+}
---
+++ accountservice/service/handlers.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/vinhnguyenhq/goblog/accountservice/dbclient"
+)
+
+var DBClient dbclient.IBoltClient
+
+func GetAccount(w http.ResponseWriter, r *http.Request) {
+
+	// Read the 'accountId' path parameter from the mux map
+	var accountId = mux.Vars(r)["accountId"]
+
+	// Read the account struct BoltDB
+	account, err := DBClient.QueryAccount(accountId)
+
+	// If err, return a 404
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// If found, marshal into JSON, write headers and content
+	data, _ := json.Marshal(account)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
